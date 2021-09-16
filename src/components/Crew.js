9af2f9bd4b4171eb24623be244c3ab66507ba151
moraelhumano <moraelhumano@gmail.com>
2021-09-16 13:44:16 -0500

    se agrega icono twitter

--- src/components/Crew.js
+++ src/components/Crew.js
@@ -32,7 +32,6 @@ class Crew extends Component{
                             </h3>
                             <p className="text-2xl text-white text-shadow-title mt-4">
                                <b>Desde Ecatepec</b> nos trae lo mejor de su humor moreno que seguramente te sacará una risa a cada trago.
-
                             </p>
                             <div className="flex justify-center md:justify-start mt-4 space-x-6">
                                 <a href="https://www.instagram.com/soyivanmendoza/?hl=es" target="_blank" className="text-white text-shadow-title">
@@ -43,6 +42,11 @@ class Crew extends Component{
                                 <a href="https://www.facebook.com/soyivanmendoza" target="_blank" className="text-white text-shadow-title">
                                     <svg className="w-8 h-8" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
                                         <path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </a>
+                                <a href="https://twitter.com/soyivanmendoza" target="_blank" className="text-white text-shadow-title">
+                                    <svg className="w-8 h-8" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
+                                        <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
                                     </svg>
                                 </a>
                             </div>
@@ -70,6 +74,11 @@ class Crew extends Component{
                                 <a href="https://www.facebook.com/lalo.elizarraras" target="_blank" className="text-white text-shadow-title">
                                     <svg className="w-8 h-8" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
                                         <path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </a>
+                                <a href="https://twitter.com/iztaparrasta" target="_blank" className="text-white text-shadow-title">
+                                    <svg className="w-8 h-8" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
+                                        <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
                                     </svg>
                                 </a>
                             </div>
@@ -103,6 +112,11 @@ class Crew extends Component{
                                 <a href="https://www.facebook.com/lalo.elizarraras" target="_blank" className="text-white text-shadow-title">
                                     <svg className="w-8 h-8" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
                                         <path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </a>
+                                <a href="https://twitter.com/iztaparrasta" target="_blank" className="text-white text-shadow-title">
+                                    <svg className="w-8 h-8" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
+                                        <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
                                     </svg>
                                 </a>
                             </div>
